a9f403b3e0648638f5744c13c4ac03bc7f5be279
Suman Mukherjee <sumanmukherjee03@gmail.com>
2013-03-15 18:52:38 -0700

    Updated the gemspec file

---
+++ lib/dispatch/rider.rb
@@ -0,0 +1,7 @@
+require "dispatch/rider/version"
+
+module Dispatch
+  module Rider
+    # Your code goes here...
+  end
+end
---
+++ lib/dispatch/rider/version.rb
@@ -0,0 +1,5 @@
+module Dispatch
+  module Rider
+    VERSION = "0.0.1"
+  end
+end
