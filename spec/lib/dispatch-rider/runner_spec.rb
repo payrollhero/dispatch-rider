a18b34ed44fc25bded87135592719c0939917de9
Suman Mukherjee <sumanmukherjee03@gmail.com>
2013-03-19 21:18:23 -0700

    Deleted a spec

--- spec/lib/dispatch-rider/runner_spec.rb
+++ spec/lib/dispatch-rider/runner_spec.rb
@@ -1,17 +1,4 @@
 require "spec_helper"
 
 describe DispatchRider::Runner do
-  before :each do
-    @demultiplexer = OpenStruct.new(:start => true, :stop => true)
-    DispatchRider::Demultiplexer.stub!(:new).and_return(@demultiplexer)
-  end
-
-  subject do
-    DispatchRider::Runner.new(DispatchRider::QueueServices::ArrayQueue.new)
-  end
-
-  it "should start the demultiplexer" do
-    @demultiplexer.should_receive(:start)
-    subject.run
-  end
 end
