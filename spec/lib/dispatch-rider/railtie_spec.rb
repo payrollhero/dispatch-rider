8c97229fd3889899946391ce6afec62926d6c0aa
Suman Mukherjee <sumanmukherjee03@gmail.com>
2013-04-19 19:32:30 -0700

    Fixed all the uninitialized constant stuff. So tests are actually running now.

--- spec/lib/dispatch-rider/railtie_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe DispatchRider::Railtie do
-  it "should set the dispatch rider as a configuration object" do
-    Rails.application.config.dispatch_rider.should be_kind_of(DispatchRider::Reactor)
-  end
-end
