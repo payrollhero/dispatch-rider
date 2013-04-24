6ee7d7d32d744a64f62f7562c1b6f79d95b8de40
Ronald Maravilla <rmaravilla@payrollhero.com>
2013-04-24 10:27:42 +0800

    Test DispatchRider::NotificationServices::AwsSns channel

--- spec/lib/dispatch-rider/notification_services/aws_sns_spec.rb
+++ spec/lib/dispatch-rider/notification_services/aws_sns_spec.rb
@@ -16,5 +16,16 @@ describe DispatchRider::NotificationServices::AwsSns do
   end
 
   describe "#channel" do
+    before { subject.stub(:channel_registrar).and_return(foo: amazon_resource_name) }
+
+    let(:topics){ mock :sns_topics }
+    let(:topic){ mock :sns_topic }
+
+    it "returns the channel" do
+      subject.notifier.should_receive(:topics).and_return(topics)
+      topics.should_receive(:[]).with(amazon_resource_name).and_return(topic)
+
+      subject.channel(:foo).should == topic
+    end
   end
 end
