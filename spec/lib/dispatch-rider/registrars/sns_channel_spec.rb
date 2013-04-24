a46bd55775d50d0458ae650112a0de956b27a020
Ronald Maravilla <rmaravilla@payrollhero.com>
2013-04-24 10:09:51 +0800

    Make SnsChannel#value return the proper Amazon Resource Name

--- spec/lib/dispatch-rider/registrars/sns_channel_spec.rb
+++ spec/lib/dispatch-rider/registrars/sns_channel_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 describe DispatchRider::Registrars::SnsChannel do
   describe "#value" do
+    let(:account){ "123456789012" }
+    let(:region){ "us-west-2" }
+    let(:topic_name){ "GeneralTopic" }
+    let(:channel_options){ {account: account, region: region, topic: topic_name} }
+
     it "returns the value for the key/value pair while registering an amazon sns channel" do
-      subject.value(:foo).should eq('foo')
+      subject.value(:foo, channel_options).should eq("arn:aws:sns:us-west-2:123456789012:GeneralTopic")
     end
   end
 end
