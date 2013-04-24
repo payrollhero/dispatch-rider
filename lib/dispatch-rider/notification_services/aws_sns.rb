496b4dc9726663984435f1f7e89fce1199aa313d
Suman Mukherjee <sumanmukherjee03@gmail.com>
2013-04-23 18:00:41 -0700

    Added some more tests for the notifcation services

--- lib/dispatch-rider/notification_services/aws_sns.rb
+++ lib/dispatch-rider/notification_services/aws_sns.rb
@@ -1,18 +1,18 @@
 module DispatchRider
   module NotificationServices
     class AwsSns < Base
-      def assign_notifier
-        @notifier = AWS::SNS.method(:new)
+      def notifier_builder
+        AWS::SNS
       rescue NameError
         raise AdapterNotFoundError.new(self.class.name, 'aws-sdk')
       end
 
-      def assign_channel_registrar
-        Registrars::SnsChannel.new
+      def channel_registrar_builder
+        Registrars::SnsChannel
       end
 
       def channel(name)
-        notifier.call.topics[self.fetch(name)]
+        notifier.topics[self.fetch(name)]
       end
     end
   end
--- spec/lib/dispatch-rider/notification_services/aws_sns_spec.rb
+++ spec/lib/dispatch-rider/notification_services/aws_sns_spec.rb
@@ -1,32 +1,20 @@
 require 'spec_helper'
 
 describe DispatchRider::NotificationServices::AwsSns do
-
   let(:amazon_resource_name){ "arn:aws:sns:us-west-2:123456789012:GeneralTopic" }
 
-  subject(:aws_sns_queue) do
-    DispatchRider::NotificationServices::AwsSns.new
+  describe "#notifier_builder" do
+    it "returns the notifier builder" do
+      subject.notifier_builder.should eq(AWS::SNS)
+    end
   end
 
-  #describe "#insert" do
-    #before do
-      #described_class.stub(:sns_constructor) do
-        #proc { mock(:sns_adaptor, :topics => sns_topic_collection) }
-      #end
-    #end
+  describe "#channel_registrar_builder" do
+    it "returns the channel registrar builder" do
+      subject.channel_registrar_builder.should eq(DispatchRider::Registrars::SnsChannel)
+    end
+  end
 
-    #let(:sns_topic_collection) do
-      #topic_collection = mock(:sns_topic_collection)
-      #topic_collection.stub(:[]){ sns_topic }
-      #topic_collection
-    #end
-    #let(:sns_topic){ mock("AWS::SNS::Topic") }
-    #let(:item) { {'subject' => 'foo', 'body' => 'bar'}.to_json }
-
-    #it "should insert a serialized object into the queue" do
-      #sns_topic.should_receive(:publish).with(item)
-
-      #aws_sns_queue.insert(item)
-    #end
-  #end
+  describe "#channel" do
+  end
 end
