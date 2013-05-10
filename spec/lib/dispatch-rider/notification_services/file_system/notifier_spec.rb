8a8bc120cd83d43f8ab778815ef9053cb71ae90e
Piotr Banasik <piotr.banasik@gmail.com>
2013-05-10 15:41:07 -0700

    fixing path to be more CI friendly

--- spec/lib/dispatch-rider/notification_services/file_system/notifier_spec.rb
+++ spec/lib/dispatch-rider/notification_services/file_system/notifier_spec.rb
@@ -5,8 +5,8 @@ describe DispatchRider::NotificationServices::FileSystem::Notifier do
   subject { described_class.new({}) }
 
   describe "#channel" do
-    example do
-      subject.channel("/some/path").should be_a(DispatchRider::NotificationServices::FileSystem::Channel)
+    it "returns a channel object" do
+      subject.channel("some/path").should be_a(DispatchRider::NotificationServices::FileSystem::Channel)
     end
   end
 
