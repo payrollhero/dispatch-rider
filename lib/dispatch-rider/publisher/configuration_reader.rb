2f270704041075e6d7748eded02179641bd800ef
Piotr Banasik <piotr.banasik@gmail.com>
2013-05-22 15:31:52 -0700

    Couple of tweaks to address @sumanmukherjee03's comments

--- lib/dispatch-rider/publisher/configuration_reader.rb
+++ lib/dispatch-rider/publisher/configuration_reader.rb
@@ -6,22 +6,22 @@ module DispatchRider
 
       class << self
         def parse(configuration_hash, publisher)
-          configure_notification_services(configuration_hash, publisher)
-          configure_destinations(configuration_hash, publisher)
+          configure_notification_services(configuration_hash[:notification_services], publisher) if configuration_hash[:notification_services]
+          configure_destinations(configuration_hash[:destinations], publisher) if configuration_hash[:destinations]
         end
 
         private
 
           def configure_notification_services(configuration_hash, publisher)
-            configuration_hash[:notification_services].each do |(service_name, service_options)|
+            configuration_hash.each do |(service_name, service_options)|
               publisher.register_notification_service(service_name, service_options)
-            end if configuration_hash[:notification_services]
+            end
           end
 
           def configure_destinations(configuration_hash, publisher)
-            configuration_hash[:destinations].each do |(destination_name, info)|
+            configuration_hash.each do |(destination_name, info)|
               publisher.register_destination(destination_name, info[:service], info[:channel], info[:options])
-            end if configuration_hash[:destinations]
+            end
           end
 
       end
