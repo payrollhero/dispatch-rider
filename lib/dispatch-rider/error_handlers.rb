4fccdf5e29ba7768f25a634169889432a99abfb7
Piotr Banasik <piotr.banasik@gmail.com>
2013-11-01 12:50:41 -0700

    Fixed Airbrake support

--- lib/dispatch-rider/error_handlers.rb
+++ lib/dispatch-rider/error_handlers.rb
@@ -12,7 +12,7 @@ module DispatchRider
   # sending the mesage and environment details.
   module AirbrakeErrorHandler
     def self.call(message, exception)
-      Airbrake.notify(exception, controller: "DispatchRider", action: message.subject, parameters: message.attributes, cgi_data: ENV)
+      Airbrake.notify(exception, controller: "DispatchRider", action: message.subject, parameters: message.attributes, cgi_data: ENV.to_hash)
     end
   end
 end
