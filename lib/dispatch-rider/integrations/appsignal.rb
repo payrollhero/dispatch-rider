6461b026837e6174dc02dc05413742f6549c00a2
Piotr Banasik <piotr.banasik@gmail.com>
2014-07-21 19:59:41 +0800

    tweaks

--- lib/dispatch-rider/integrations/appsignal.rb
+++ lib/dispatch-rider/integrations/appsignal.rb
@@ -1,44 +1,43 @@
-if defined? Appsignal
-  Appsignal.logger.info('Loading Dispatch Rider integration')
+Appsignal.logger.info('Loading Dispatch Rider integration')
 
-  module DispatchRider
-    module Integrations
-      module Appsignal
+module DispatchRider
+  module Integrations
+    module Appsignal
 
-        def self.wrap_message(job, message)
-          begin
-            Appsignal::Transaction.create(SecureRandom.uuid, ENV.to_hash)
+      def self.wrap_message(message)
+        begin
+          Appsignal::Transaction.create(SecureRandom.uuid, ENV.to_hash)
 
-            ActiveSupport::Notifications.instrument(
-              'perform_job.dispatch-rider',
-              :class => message.subject,
-              :method => 'handle',
-              :attempts => message.receive_count,
-              :queue => message.queue_name,
-              :queue_time => (Time.now.to_f - message.sent_at.to_f) * 1000
-            ) do
-              job.call
-            end
-          rescue Exception => exception
-            unless Appsignal.is_ignored_exception?(exception)
-              Appsignal::Transaction.current.add_exception(exception)
-            end
-            raise exception
-          ensure
-            Appsignal::Transaction.current.complete!
+          ActiveSupport::Notifications.instrument(
+            'perform_job.dispatch-rider',
+            :class => message.subject,
+            :method => 'handle',
+            :attempts => message.receive_count,
+            :queue => message.queue_name,
+            :queue_time => (Time.now.to_f - message.sent_at.to_f) * 1000
+          ) do
+            yield
           end
+        rescue Exception => exception
+          unless Appsignal.is_ignored_exception?(exception)
+            Appsignal::Transaction.current.add_exception(exception)
+          end
+          raise exception
+        ensure
+          Appsignal::Transaction.current.complete!
         end
-
       end
+
     end
   end
+end
 
-  DispatchRider.config do |config|
+DispatchRider.config do |config|
 
-    config.around(:dispatch_message) do |job, message|
-      DispatchRider::Appsignal.wrap_message(job, message)
+  config.around(:dispatch_message) do |job, message|
+    DispatchRider::Appsignal.wrap_message(message) do
+      job.call
     end
-
   end
 
 end
