dde6174c6bffb5460cdab585f5e8aacb31709773
Vincent Paca <vincent.paca@gmail.com>
2016-02-04 17:08:44 +0800

    Update appsignal integration

--- lib/dispatch-rider/integrations/appsignal.rb
+++ lib/dispatch-rider/integrations/appsignal.rb
@@ -6,31 +6,20 @@ if defined? Appsignal
       module Appsignal
 
         def self.wrap_message(job, message)
-          begin
-            ::Appsignal::Transaction.create(
-              SecureRandom.uuid,
-              ::Appsignal::Transaction::BACKGROUND_JOB,
-              ::Appsignal::Transaction::GenericRequest.new(ENV.to_hash)
-            )
+          ::Appsignal.start
 
-            ::ActiveSupport::Notifications.instrument(
-              'perform_job.dispatch-rider',
-              :class => message.subject,
-              :method => 'handle',
-              :attempts => message.receive_count,
-              :queue => message.queue_name,
-              :queue_time => (Time.now.to_f - message.sent_at.to_f) * 1000
-            ) do
-              job.call
-            end
-          rescue Exception => exception
-            unless ::Appsignal.is_ignored_error?(exception)
-              ::Appsignal::Transaction.current.add_exception(exception)
-            end
-            raise exception
-          ensure
-            ::Appsignal::Transaction.complete_current!
+          ::Appsignal.monitor_transaction(
+            'perform_job.dispatch-rider',
+            class: message.subject,
+            method: 'handle',
+            attempts: message.receive_count,
+            queue: message.queue_name,
+            queue_time: (Time.now.to_f - message.sent_at.to_f) * 1000,
+          ) do
+            yield
           end
+
+          ::Appsignal.stop
         end
 
       end
@@ -43,10 +32,6 @@ if defined? Appsignal
       DispatchRider::Integrations::Appsignal.wrap_message(job, message)
     end
 
-  end
-
-  if ::Appsignal.active?
-    ::Appsignal.start
   end
 
 end
