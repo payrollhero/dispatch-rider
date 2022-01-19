4df64208d77bc7bdb6b8fe1f3f99a45054f4b4c4
Mathieu Jobin <mathieu@justbudget.com>
2022-01-19 10:30:42 +0900

    replace byebug/pry by debug and FactoryGirl to FactoryBot

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,10 +1,10 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :message, class: 'DispatchRider::Message' do
-    subject 'sample_handler'
-    body(
+    subject { 'sample_handler' }
+    body {{
       'key' => 'value',
       'guid' => DispatchRider::Debug::PUBLISHER_MESSAGE_GUID,
-    )
+    }}
     initialize_with {  DispatchRider::Message.new(attributes) }
   end
 end
