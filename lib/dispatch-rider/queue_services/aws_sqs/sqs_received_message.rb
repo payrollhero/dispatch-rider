652e3195b1daf71daf92db8f905b5ee66b64a753
Mathieu Jobin <mathieu@justbudget.com>
2022-12-08 07:12:05 +0900

    use change visibility

--- lib/dispatch-rider/queue_services/aws_sqs/sqs_received_message.rb
+++ lib/dispatch-rider/queue_services/aws_sqs/sqs_received_message.rb
@@ -16,8 +16,9 @@ module DispatchRider
         # NOTE: Setting the visibility timeout resets the timeout to NOW and makes it visibility timeout this time
         # Essentially resetting the timer on this message
         def extend_timeout(timeout)
-          item.visibility_timeout = timeout
-        ensure
+          item.change_visibility({
+            visibility_timeout: timeout # required
+          })
           @total_timeout = timeout + (Time.now - start_time).to_i if timeout > 0
         end
 
