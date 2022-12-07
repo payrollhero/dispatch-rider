e044c520175d9c8a32382feca0a683db7992f7a5
Rubocop Challenger <rubocop@payrollhero.com>
2022-12-07 23:33:11 +0000

    :police_car: Style/ClassAndModuleChildren

--- lib/dispatch-rider/publisher/configuration.rb
+++ lib/dispatch-rider/publisher/configuration.rb
@@ -1,45 +1,47 @@
 # frozen_string_literal: true
 
 module DispatchRider
-  class Publisher::Configuration
-    def initialize(configuration_hash = {})
-      @notification_services = []
-      @destinations = []
-      parse(configuration_hash)
-    end
-
-    def notification_services
-      @notification_services
-    end
-
-    def destinations
-      @destinations
-    end
-
-    def parse(configuration_hash)
-      clear
-
-      configuration_hash = configuration_hash.with_indifferent_access
-      configure_notification_services(configuration_hash[:notification_services] || {})
-      configure_destinations(configuration_hash[:destinations] || {})
-    end
-
-    def clear
-      @notification_services.clear
-      @destinations.clear
-    end
-
-    private
-
-    def configure_notification_services(notification_services_hash)
-      notification_services_hash.each do |name, options|
-        @notification_services << NotificationService.new(name, options)
+  module Publisher
+    class Configuration
+      def initialize(configuration_hash = {})
+        @notification_services = []
+        @destinations = []
+        parse(configuration_hash)
       end
-    end
 
-    def configure_destinations(destinations_hash)
-      destinations_hash.each do |name, options|
-        @destinations << Destination.new(name, options)
+      def notification_services
+        @notification_services
+      end
+
+      def destinations
+        @destinations
+      end
+
+      def parse(configuration_hash)
+        clear
+
+        configuration_hash = configuration_hash.with_indifferent_access
+        configure_notification_services(configuration_hash[:notification_services] || {})
+        configure_destinations(configuration_hash[:destinations] || {})
+      end
+
+      def clear
+        @notification_services.clear
+        @destinations.clear
+      end
+
+      private
+
+      def configure_notification_services(notification_services_hash)
+        notification_services_hash.each do |name, options|
+          @notification_services << NotificationService.new(name, options)
+        end
+      end
+
+      def configure_destinations(destinations_hash)
+        destinations_hash.each do |name, options|
+          @destinations << Destination.new(name, options)
+        end
       end
     end
   end
--- lib/dispatch-rider/publisher/configuration/notification_service.rb
+++ lib/dispatch-rider/publisher/configuration/notification_service.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 module DispatchRider
-  class Publisher::Configuration::NotificationService
-    def initialize(name, options)
-      @name = name
-      @options = options
-    end
+  module Publisher
+    module Configuration
+      class NotificationService
+        def initialize(name, options)
+          @name = name
+          @options = options
+        end
 
-    attr_reader :name, :options
+        attr_reader :name, :options
 
-    def ==(other)
-      name == other.name &&
-        options == other.options
+        def ==(other)
+          name == other.name &&
+            options == other.options
+        end
+      end
     end
   end
 end
