cff581a1122b3b79f1eb680a2e4673067e839039
Rubocop Challenger <rubocop@payrollhero.com>
2023-06-08 23:33:56 +0000

    :police_car: Layout/SpaceAroundEqualsInParameterDefault

--- lib/dispatch-rider/publisher/configuration/destination.rb
+++ lib/dispatch-rider/publisher/configuration/destination.rb
@@ -2,7 +2,7 @@
 
 module DispatchRider
   class Publisher::Configuration::Destination
-    def initialize(name, attributes={})
+    def initialize(name, attributes = {})
       @name = name
 
       attributes = attributes.with_indifferent_access
